fad39f69a389382791457ede839d66899bd61bcb
tgoddessana <twicegoddessana1229@gmail.com>
2022-10-30 11:57:55 +0900

    Project Initial Commit

---
+++ src/main/java/StandardJava/Chapter02/Ex_Variable.java
@@ -0,0 +1,16 @@
+package StandardJava.Chapter02;
+
+/**
+ * 변수 : 하나의 값을 저장하기 위한 공간
+ * 상수 : 값을 한 번만 저장할 수 있는 공간
+ * 리터럴 : 그 자체로 값을 의미하는 것
+ */
+public class Ex_Variable {
+    public static void main(String[] args) {
+        // 변수 선언 : 변수타입 변수이름;
+        int someInteger = 1229;
+
+        // 상수 선언 : 변수 타입 앞에 키워드 'final' 붙임
+        final int someFinalInteger = 9221;
+    }
+}
